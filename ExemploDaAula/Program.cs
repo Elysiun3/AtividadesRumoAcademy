763cd47e3449176a64e3ff419ab5cc18fde105f2
Elysiun3 <savio.h.martins@gmail.com>
2023-01-11 10:02:16 -0300

    atividade1PortugolParaC#

---
+++ ExemploDaAula/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExemploDaAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!!!!");
+
+
+            var matrizNumeros = new int[1, 2];
+
+            matrizNumeros[0, 0] = 1;
+            matrizNumeros[0, 1] = 4;
+            matrizNumeros[0, 2] = 67;
+
+            matrizNumeros[1, 0] = 1;
+            matrizNumeros[1, 1] = 1;
+            matrizNumeros[1, 2] = 1;
+        }
+    }
+}
